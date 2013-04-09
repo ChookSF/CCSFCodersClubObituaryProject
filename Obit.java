540e97128d53acb99269a3d3113cc45d6260b090
Thomas Charles Griffin <thomas.charles.griffin@gmail.com>
2013-04-09 00:09:38 -0700

    Fairly Cleaned Up
    finished the phrase search frequency method and cleaned up the
    documentation.

--- Obit.java
+++ Obit.java
@@ -2,11 +2,9 @@
 import java.io.*;
 import java.util.*;
 
-
-
-
 class Obit
 {
+// Declare attributes for the Obit Class
    private String filename;
    private String filepath;
    private String name;
@@ -19,22 +17,31 @@ class Obit
    private String monthStr;
 
 
-   Obit(File f)
+   Obit(File f)    // **** Constructor *****
    {
+	  // Set the data file information
       setFilename(f.getName());
       setFilepath(f.getPath());
       
-      pubDate = filename.substring(0,8);
-      int year = Integer.parseInt(pubDate.substring(0,4));
+      // Gather date information
+      // date info can be accesed by int, string or Calendar class 
+      // so we can decide how to use it later.
+      pubDate   = filename.substring(0,8);
+      int year  = Integer.parseInt(pubDate.substring(0,4));
       int month = Integer.parseInt(pubDate.substring(4,6));
-      int day = Integer.parseInt(pubDate.substring(6));
- 
+      int day   = Integer.parseInt(pubDate.substring(6));
       String datestr = (year + "/" + month + "/" + day);
-      monthStr = (month + "/" + year);
+      String leadingZed = "";
+      if (month < 10) leadingZed = "0";
+      monthStr = ( year + "/" +leadingZed + month);
       date.clear();
       date.set(year, month, day);
       
+      // get the name of the deceased 
       name = filename.substring(9, filename.length() - 4);
+      name = name.replace(".jpg", "");
+      
+      // Get the text of the obituary and load it into the class
       String fileText = "";
       Scanner sc;    
       try
@@ -54,15 +61,19 @@ class Obit
       // System.out.println(fileText); 
       // System.out.println(date);
       
-       setObitText(fileText.replace('\n', ' '));
+      // remove the newline charcaters so the text is one long string
+      setObitText(fileText.replace('\n', ' '));
 
-    this.obitText = fileText.toLowerCase();
-    this.obitText = obitText.replaceAll("[.,:!;\"{}()?]", "");
-    this.obitText = obitText.trim();
-    this.wordCount = setWordCount(obitText);
-            
+      // clean the text so it is easier to handle
+      this.obitText = fileText.toLowerCase();
+      this.obitText = obitText.replaceAll("[.'&,:!;\"{}()?]", "");
+      this.obitText = obitText.trim();
+      this.wordCount = setWordCount(obitText);          
    }
 
+   
+   // This method returns a hash map that has all of the words in the
+   // obituary and how many times they appear.
    private HashMap<String, Integer> setWordCount(String txt)
    {
       String[] words = txt.split(" ");
@@ -70,24 +81,19 @@ class Obit
 
       for (String word : words)
       {
-    	 if(! word.equals(" ") || ! word.equals(null))
+         if(cnt.containsKey(word))
          {
-    	    if(cnt.containsKey(word))
-            {
-               int n = cnt.get(word);
-               n++;
-               cnt.put(word, n);
-            }
-            else 
-               cnt.put(word, 1);
+            int n = cnt.get(word);
+            n++;
+            cnt.put(word, n);
          }
-    	 cnt.remove("");
-    	 cnt.remove("-");
-    	 
+         else 
+            cnt.put(word, 1);    	 
       }
       return cnt;
    }
 
+   // The toString method is to print out the Obit object as needed
    public String toString()
    {
       HashMap<String, Integer> wordCount = getWordCount();
@@ -106,6 +112,8 @@ class Obit
       return (str);
    }
 
+   // *** SETTERS AND GETTERS ****
+   
    public String getFilename() {return filename;}
    public String getFilepath() {return filepath;}
    public String getName() {return name;}
@@ -126,5 +134,5 @@ class Obit
    public void setWordCount(HashMap<String, Integer> wordCount) {this.wordCount = wordCount;}
    public void setAdjustedWordCount(HashMap<String, Integer> adjustedWordCount) {this.adjustedWordCount = adjustedWordCount;}
    public void setDate(Calendar date) {this.date = date;}
-   public void setMonthStr(String str) {this.monthStr = monthStr;}
+   public void setMonthStr(String monthStr) {this.monthStr = monthStr;}
 }
--- ObitMain.java
+++ ObitMain.java
@@ -5,131 +5,174 @@ import java.util.*;
 
 public class ObitMain   
 {
+   static ArrayList<Obit> obits = new ArrayList<Obit>();  // Container for holding all of the obituaries
+   static TreeMap<String, Integer> sortedTally  = new TreeMap<String, Integer>();
+   
    public static void main(String[] args) 
    {
-      File[] dataDirs, obitFiles;
-      ArrayList<Obit> obits = new ArrayList<Obit>();
-      String monthsList[] = mkMonthListArray();
+      File[] dataDirs, obitFiles;                     // Arrays to hold data files and data directories
+      String monthsList[] = mkMonthListArray();       // list of possible month/year combinations 
 
-      // get list of directories in the data folder
-    String obitDataDir = "C:\\Users\\Chuck\\Dropbox\\workspace\\Obits\\src\\ObitDataFiles";
-  //    String obitDataDir = "/home/chuck/Dropbox/workspace/Obits/src/ObitDataFiles";
-      File dataDir = new File(obitDataDir);
-     
-      dataDirs = dataDir.listFiles();
+      
+    // Chose your data directory      
+    String obitDataDir = "C:\\Users\\Chuck\\Dropbox\\workspace\\Obits\\src\\ObitDataFiles2";
+  //   String obitDataDir = "/home/chuck/Dropbox/workspace/Obits/src/ObitDataFiles2";
 
-      // Load up the obituaries in to an array list 
-      for (File f : dataDirs) 
-      {
-       //System.out.println(f.getName());
-         System.out.println("");
-         System.out.println(f.getPath());
-         
-         obitFiles = f.listFiles();
-
-         for (File obit : obitFiles)
+    
+      File dataDir = new File(obitDataDir);     // create file object of the data directory
+      dataDirs = dataDir.listFiles();           // get array of each of the data sub-directories
+      
+      for (File subDir : dataDirs)                   // for each sub-directory: 
+      {                                         
+         //System.out.println(subDir.getPath());       // print out the path of the sub-directory
+         obitFiles = subDir.listFiles();               // get an array of every file in the sub-directory
+         System.out.println("loading "+ obitFiles.length + " from the year " + subDir.getName());
+         for (File dataFile : obitFiles)               // for each data file: 
          {
-        	 System.out.println(obit.getName());
-         
-        	 obits.add(new Obit(obit));
-         }
+        //	 System.out.println(dataFile.getName());   // print the file name monitoring
+        	 obits.add(new Obit(dataFile));            // create an Obit object from the data file 
+         }                                             // and add it to the Obits Array List
       }
       
-      // Print out data about each obituary
-      for (Obit obit : obits) 
-      {
-    	  Calendar date = obit.getDate();
-    	  int year = date.get(Calendar.YEAR); 
-    	  int month = date.get(Calendar.MONTH);
-    	  int day = date.get(Calendar.DAY_OF_MONTH);
-    	  String dateStr = (month +"/"+day+"/"+year);
-    	  
-    	  System.out.println(obit.getName() + " " + dateStr +
-    			             "  " + obit.getPubDate() + " -- -- -- "+obit.getObitText());
-      }
-      
-    //  System.out.println("Death Toll: " + obits.size());
-      
-    // (String my : monthsList) System.out.println(my);
-
-   //   wordByMonth(obits,"aids");
-
+   // printAllObits(obits);                               
+      System.out.println("Death Toll: "+obits.size()); // show how many files were processed
+ 
+      sortedTally = tallyWordCounts(obits);    
       
       
+     printAlphabeticTally(sortedTally, 100);
+  
+     // lets do some phrase tallies
+     phraseByMonth("lover");
+     phraseByMonth("celebration of life");
+     phraseByMonth("aidsrelated");
+     phraseByMonth("cancer");
+     phraseByMonth("longtime companion");
+     phraseByMonth("illness");
+     phraseByMonth("aids");
+     phraseByMonth("love");
+     
+     
+     
       
-      // Tally the words in all the abutters
-       
-      HashMap<String, Integer> tally = new HashMap<String, Integer>();
-  	  TreeMap<String, Integer> sortedTally = new TreeMap<String, Integer>();
-  	  
-      tally = tallyWordCounts(obits);
-      sortedTally.putAll(tally);
-      
-      /*
-      for (String word : sortedTally.keySet()) 
-    	  if (tally.get(word) > 5)
-    	     System.out.println(word + " - " + sortedTally.get(word));
-  */
-      
-      
-      
-      
-      
-   }
-   
-   private static HashMap<String, Integer> tallyWordCounts(ArrayList<Obit> obits)
+  }  
+   // This method checks the obits array and prints out a list of mm/yyyy and the 
+   // frequency the given word appears.
+   static void phraseByMonth(String word)
+   //static void phraseByMonth(ArrayList<Obit> obits, String word)
    {
-	   HashMap<String, Integer> tally = new HashMap<String, Integer>();
+	   TreeMap<String, Integer> monthCount = new TreeMap<String, Integer>();
+
+	   for (Obit obit : obits)
+	   {
+		  String obitTxt = obit.getObitText();
+		  String mmyyyy = obit.getMonthStr();
+		  
+		  if (obitTxt.contains(word)) 
+		  {
+			  
+			  if(monthCount.containsKey(mmyyyy))
+	            {
+	               int n = monthCount.get(mmyyyy);
+	               n++;
+	               monthCount.put(mmyyyy, n);
+	            }
+	            else 
+	               monthCount.put(mmyyyy, 1);
+		  }
+	   }
+	   
+	   System.out.println("");
+	   System.out.println("***** " + word + "*****");
+	   System.out.println("The word '" + word +"' appears " + sortedTally.get(word) + " times.");
+	   for (String mmyyyy : monthCount.keySet()) 
+		   System.out.print(mmyyyy + " - " + monthCount.get(mmyyyy) + "\n");
+   }
+      
+   
+   // Print out data about each obituary on a single line
+   // The Obit class has a toString method, but this method can be
+   // used to print out the obituary on a single line.
+   // It's main purpose is to check the obituaries for obvious 
+   // errors in loading. 
+   public static void printAllObits(ArrayList<Obit> obits)
+   {
+     for (Obit obit : obits) 
+     {
+ 	    Calendar date = obit.getDate();
+ 	    int year = date.get(Calendar.YEAR); 
+ 	    int month = date.get(Calendar.MONTH);
+ 	    int day = date.get(Calendar.DAY_OF_MONTH);
+ 	    String dateStr = (month +"/"+day+"/"+year);
+ 	  
+ 	    System.out.println(obit.getName() + " " + dateStr +
+ 		 	             "  " + obit.getPubDate() + " -- -- -- "+obit.getObitText());
+     }
+   } 
+   
+   // prints out the contents of the sorted word tally with a control integer 
+   // to limit less frequent word counts.
+   public static void printAlphabeticTally(TreeMap<String, Integer> sortedTally, int min)
+   {
+	   int cnt = 0;
+      for (String word : sortedTally.keySet())
+      {
+ 	     if (sortedTally.get(word) > min)
+ 	     {
+ 	        System.out.println(word + " - " + sortedTally.get(word)); 
+ 	        cnt++;
+ 	     }
+      }
+      System.out.println(cnt + " words appear more than " + min + " times.");
+    }
+   
+   // This method returns an alphabetic tree map of all of the words found in 
+   // all of the obituaries and the number of times each word appears.
+   private static TreeMap<String, Integer> tallyWordCounts(ArrayList<Obit> obits)
+   {
+	   TreeMap<String, Integer> tally = new TreeMap<String, Integer>();
 	   HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
 	   
 	   for (Obit obit : obits)
 	   {
 		   wordCount = obit.getWordCount();
-		   
 		   Set<String> k = wordCount.keySet();
-		  // System.out.println(k);
 		   
-			  for (String word : k) 
-		      {
-				 int x = 0, y = 0;
-				 int z;
-				 
-				 if (tally.containsKey(word)) x = tally.get(word); 
-				 if (wordCount.containsKey(word)) y = wordCount.get(word);
-				 z = x + y;
-				 tally.put(word, z);
-		      }
-		 
+		  for (String word : k) 
+	      {
+			 int x = 0, y = 0;
+			 int z;
+			 
+			 if (tally.containsKey(word)) x = tally.get(word); 
+			 if (wordCount.containsKey(word)) y = wordCount.get(word);
+			 z = x + y;
+			 tally.put(word, z);
+	      }
 		}	   
 	   return (tally);
    }
 
+   // This method creates a list of month and year combinations 
+   // in the form of mm/yyyy.  The purpose of this method "just in case"
+   // we need a listing of the months, which I think we will.
    static String[] mkMonthListArray()
    {
 	   String[] monthList = new String[575];
 	   int month = 1;
 	   int year = 1970;
+	   String leadingZed ="";
 	   
 	   for (int i = 0; i < monthList.length ; i++)
 	   {
 		   if (++month % 12 == 0) year++;
-		   monthList[i] = (month % 12 + 1) + "/" + year;
+		   if (month %12 + 1 < 10) leadingZed = "0";
+		   monthList[i] =    year + "/" + (month % 12 + 1);
+		 // System.out.println(monthList[i]);
 	   }
 	   return (monthList);
    }
    
-   static void wordByMonth(ArrayList<Obit> obits, String word)
-   {
-	   HashMap<String, Integer> monthCount = new HashMap<String, Integer>();
 
-	   for (Obit obit : obits)
-	   {
-		  String obitTxt = obit.getObitText();
-		  System.out.println(obitTxt);
-	   }
-	   
-   }
-   
    
    
    
