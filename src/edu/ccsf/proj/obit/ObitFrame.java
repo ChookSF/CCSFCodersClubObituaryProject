d2ef90321285975dedce99b83b5d207095c6a109
Victor <vmalchik@mail.ccsf.edu>
2013-04-16 18:47:08 -0700

    Comments added to ObitFrame class

--- src/edu/ccsf/proj/obit/ObitFrame.java
+++ src/edu/ccsf/proj/obit/ObitFrame.java
@@ -7,6 +7,8 @@ public class ObitFrame extends JFrame
 	public ObitFrame() 
 	{
 		setTitle("Obit Frame");
+		//note: size of the screen should be based off user's 
+		//      monitor/screen size
 		setSize(350, 350);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
