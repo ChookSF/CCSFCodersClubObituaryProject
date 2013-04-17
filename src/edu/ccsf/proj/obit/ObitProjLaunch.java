1c104b0372b0aa147e25e1e22e445c5939140d8b
Victor <vmalchik@mail.ccsf.edu>
2013-04-16 18:23:06 -0700

    GUI Launching Prog
    This program will launch the gui

---
+++ src/edu/ccsf/proj/obit/ObitProjLaunch.java
@@ -0,0 +1,25 @@
+package edu.ccsf.proj.obit;
+
+import javax.swing.SwingUtilities;
+
+public class ObitProjLaunch 
+{
+	public static void main(String[] args)
+	{
+		try 
+		{
+			SwingUtilities.invokeLater(new Runnable() //create thread for Swing
+			{
+				public void run() //implement run
+				{
+					
+				}
+			});
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+}
