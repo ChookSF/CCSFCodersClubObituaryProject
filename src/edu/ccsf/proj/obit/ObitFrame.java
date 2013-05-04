ec44b35f6a8a9815fcb1e13a26caa30f9ca4b697
victor <victormalchikov@yahoo.com>
2013-05-03 19:19:00 -0700

    GUI - Created WordListTab
    Added a few gui elements. Very basic update.

--- src/edu/ccsf/proj/obit/ObitFrame.java
+++ src/edu/ccsf/proj/obit/ObitFrame.java
@@ -1,6 +1,9 @@
 package edu.ccsf.proj.obit;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+import edu.ccsf.proj.obit.tabs.WordListTab;
 
 public class ObitFrame extends JFrame
 {
@@ -10,6 +13,13 @@ public class ObitFrame extends JFrame
 		//note: size of the screen should be based off user's 
 		//      monitor/screen size
 		setSize(350, 350);
+		
+		//tabs
+		JTabbedPane tp = new JTabbedPane();
+		tp.addTab("Word List", new WordListTab());
+		
+		getContentPane().add(tp);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
---
+++ src/edu/ccsf/proj/obit/tabs/WordListTab.java
@@ -0,0 +1,11 @@
+package edu.ccsf.proj.obit.tabs;
+
+import javax.swing.JPanel;
+
+public class WordListTab extends JPanel
+{
+	public WordListTab()
+	{
+		
+	}
+}
